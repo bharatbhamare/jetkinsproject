d52af1838ed98eb510c8ea2653f3d0554866a143
shubham.shirore <shubham.shirore@Dev12.glodom.local>
2018-09-29 17:26:13 +0530

    changed

--- first-jetkins-prohect/src/main/java/com/test/firstjetkinsprohect/Contrller.java
+++ first-jetkins-prohect/src/main/java/com/test/firstjetkinsprohect/Contrller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Contrller {
 
-	@GetMapping("/")
+	@GetMapping("/getHello")
 	public String SayHello()
 	{
 		return "hello shubham";
