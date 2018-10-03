b65309735bd1536e776312f867e99d9e3c1c4898
shubham.shirore <shubham.shirore@Dev12.glodom.local>
2018-10-03 10:06:22 +0530

    commited

--- first-jetkins-prohect/src/main/java/com/test/firstjetkinsprohect/FirstJetkinsProhectApplication.java
+++ first-jetkins-prohect/src/main/java/com/test/firstjetkinsprohect/FirstJetkinsProhectApplication.java
@@ -1,12 +1,19 @@
 package com.test.firstjetkinsprohect;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FirstJetkinsProhectApplication {
+public class FirstJetkinsProhectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstJetkinsProhectApplication.class, args);
 	}
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(FirstJetkinsProhectApplication.class);
+	    }
 }
